c3af06fa61f34aae365d83a34c72256bdfae9c4a
Eivin Landa <landa@drivdigital.no>
2014-09-09 21:27:26 +0200

    Un-broke the menu with some duct tape

--- menu.js
+++ menu.js
@@ -3,7 +3,7 @@ voidjs.menu = {};
 
 
 (function(menu) {
-  var m = {};
+  // var m = {};
   // Small helper jquery function
   $.fn.m_click = function(action) {
     $(this).click(function() {
@@ -34,6 +34,7 @@ voidjs.menu = {};
   // #3 : Control options
   // #4 : Progression status
   // #5 : Game settings??
+  /*
   m.complete = $('<div>').hide().append(
     $('<ul>').append(
       $('<li>')
@@ -81,34 +82,8 @@ voidjs.menu = {};
   for (var x in m) {
     $('#menu').append(m[x]);
   }
+  */
 
-  menu.show = function(part) {
-    var elements = [];
-    switch (part) {
-      case 'LevelComplete':
-        m.complete.show();
-        break;
-      case 'SelectLevel':
-        m.level_select.show();
-        break;
-      case 'Pause':
-        m.pause.show();
-        break;
-      case 'Other':
-      break;
-
-      default:
-        $('#main-menu').show();
-        break;
-    }
-
-    if (!$('li.selected:visible').length) {
-      $('li:visible:eq(0)').addClass('selected');
-    }
-
-    vcore.invoke('menu_show', part);
-  }; // - show menu
-  menu.hide = $('.menu').hide;
 
   menu.next = function() {
     if (!$('li.selected:visible').length) {
@@ -137,8 +112,89 @@ voidjs.menu = {};
     vcore.invoke('menu_select');
   };
 
-} )( voidjs.menu );
+// } )//( voidjs.menu );
+// ( function( menu ) {
+  var container = $('#menu-container');
+  var book_list = $('<ul>');
+  var chapter_list = $('<ul>');
+  var section_list = $('<ul>');
 
+  var book1 = $('<li>');
+  var chapter1 = $('<li>');
+  var section1 = $('<li>');
+
+  var variables = {
+    on: {
+      book: $('<span>').text('Book 1: The first book'),
+      chapter: $('<span>').text('Chapter 1: The first chapter'),
+      section: $('<span>').text('Section 1: The first section')
+    },
+    list: {
+      book: book_list,
+      chapter: chapter_list,
+      section: section_list
+    }
+  };
+
+  menu.hide = function() { container.hide(); };
+  container.html( function( index, oldHtml ) {
+    return oldHtml.replace( /{{/g, '<span class="placeholder">').replace( /}}/g, '</span>')
+  } );
+
+  $( '.placeholder', container ).replaceWith( function() {
+    var parts = $(this).text().split(':');
+    return variables[parts[0]][parts[1]];
+  } );
+
+  var catalog = {
+    main: $('#main-menu'),
+    level_complete: $('#level-complete'),
+    level_select: $('#level-select'),
+    pause: $('#pause'),
+  };
+
+  menu.show = function(part) {
+  $('.menu').hide();
+    container.show();
+    var elements = [];
+    var active_menu = false;
+    switch (part) {
+      case 'LevelComplete':
+        active_menu = catalog.level_complete;
+        break;
+      case 'SelectLevel':
+        active_menu = catalog.level_select;
+        break;
+      case 'Pause':
+        active_menu = catalog.pause;
+        break;
+      case 'Other':
+      break;
+
+      case 'Main':
+      default:
+        active_menu = catalog.main;
+        break;
+    }
+
+    if ( active_menu ) {
+      active_menu.show();
+    }
+
+    if (!$('li.selected:visible').length) {
+      $('li:visible:eq(0)').addClass('selected');
+    }
+
+    vcore.invoke('menu_show', part);
+  }; // - show menu
+
+
+  $('[data-action="continue_play"] a').m_click(
+    function() {
+      voidjs.goto('game', 1);
+    });
+  $('[data-action="select_level"] a').m_click( menu.hide );
+} ( voidjs.menu ) );
 
 // Implement a hook to move around the menu with controllers
 vcore.hook( 'player_action', function(args) {
